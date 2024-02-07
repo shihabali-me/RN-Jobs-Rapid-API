7e8dad42a6366d258c4d3a9c36eddbaf5b9ca96d
shihabali-me <shihabali.me@gmail.com>
2024-02-07 17:16:12 +0600

    completed all feature

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const jobTypes = ["Full-time", "Part-time", "Internship"]
+
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full Time");
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Welcome</Text>
+        <Text style={styles.welcomeMessage}>Find Your Perfect Job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder='What are you looking for?'
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <TouchableOpacity 
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+                <Text style={styles.tabText(activeJobType, item)}>
+                  {item}
+                </Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
